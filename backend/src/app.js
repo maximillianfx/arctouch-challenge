095b93d7a760058d4759a008b214f40dc297bb57
Maximillian Xavier <maximillianfx@gmail.com>
2020-03-12 15:06:34 -0300

    Separate methods to get poster and backdrop, add movies search

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,28 @@ var app = express();
 
 var API_KEY = '1f54bd990f1cdfb230adb312546d765d';
 var URL_BASE = 'https://api.themoviedb.org/3/movie/';
-var URL_BASE_IMAGE = 'https://image.tmdb.org/t/p/w780/';
+var URL_SEARCH = 'https://api.themoviedb.org/3/search/movie';
+var URL_BASE_IMAGE_POSTER = 'https://image.tmdb.org/t/p/w342/';
+var URL_BASE_IMAGE_BACKDROP = 'https://image.tmdb.org/t/p/w780/';
 var LANGUAGE = 'es-US';
+
+app.get('/movies', function (req, res) {
+    if (!req.query.search) {
+        res.send({ code: 404, data: 'The search needs a valid string. Ex.: /api/v1/movies?search=text' });
+    } else if(!req.query.page || !parseInt(req.query.page)) {
+        res.send({ code: 404, data: 'Page must be a integer' });
+    } else {
+        axios.get(URL_SEARCH + '?api_key=' + API_KEY + '&language=' + LANGUAGE + '&query=' + req.query.search + '&page=' + req.query.page)
+            .then(function (response) {
+                //Handle success request
+                res.send({ code: response.status, data: response.data });
+            })
+            .catch(function (error) {
+                // Handle error from request
+                res.send({ code: error.response.status, data: error.response.data.status_message })
+            });
+    }
+});
 
 app.get('/movies/upcoming', function (req, res) {
     if (!req.query.page || !parseInt(req.query.page)) {
@@ -26,7 +46,7 @@ app.get('/movies/upcoming', function (req, res) {
 
 app.get('/movies/:id', function (req, res) {
     if (!req.params.id || !parseInt(req.params.id)) {
-        res.send({ code: 404, data: 'ID must be a integer' })
+        res.send({ code: 404, data: 'ID must be an integer' })
     } else {
         axios.get(URL_BASE + req.params.id + '?api_key=' + API_KEY + '&language=' + LANGUAGE)
             .then(function (response) {
@@ -40,25 +60,48 @@ app.get('/movies/:id', function (req, res) {
     }
 });
 
-app.get('/movies/:id/:imageLink', function (req, res) {
+app.get('/backdrop/:imageLink', function (req, res) {
 
-    const imageFile = req.params.imageLink;
-    axios.get(URL_BASE_IMAGE + imageFile,
-        {
-            responseType: 'arraybuffer'
-        }).then((response) => {
-            //Handle the success request for image poster or backdrop
-            res.send({ code: response.status, data: Buffer.from(response.data, 'binary').toString('base64') });
-        }).catch(function (error) {
-            // Handle error from request
-            res.send({ code: error.response.status, data: error.message })
-        });
+    if (!req.params.imageLink) {
+        res.send({ code: 404, data: 'Invalid imagelink' });
+    } else {
+
+        const imageFile = req.params.imageLink;
+        axios.get(URL_BASE_IMAGE_BACKDROP + imageFile,
+            {
+                responseType: 'arraybuffer'
+            }).then((response) => {
+                //Handle the success request for image poster or backdrop
+                res.send({ code: response.status, data: Buffer.from(response.data, 'binary').toString('base64') });
+            }).catch(function (error) {
+                // Handle error from request
+                res.send({ code: error.response.status, data: error.message })
+            });
+
+    }
+});
+
+app.get('/poster/:imageLink', function (req, res) {
+
+    if (!req.params.imageLink) {
+        res.send({ code: 404, data: 'ID must be an integer/Invalid imagelink' });
+    } else {
+
+        const imageFile = req.params.imageLink;
+        axios.get(URL_BASE_IMAGE_POSTER + imageFile,
+            {
+                responseType: 'arraybuffer'
+            }).then((response) => {
+                //Handle the success request for image poster or backdrop
+                res.send({ code: response.status, data: Buffer.from(response.data, 'binary').toString('base64') });
+            }).catch(function (error) {
+                // Handle error from request
+                res.send({ code: error.response.status, data: error.message })
+            });
+
+    }
 });
 
 app.listen(3000, function () {
     console.log('Arctouch App listening on port 3000!');
-});
-
-app.use(function (req, res) {
-    res.sendStatus(404);
 });
