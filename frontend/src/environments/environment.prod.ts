d523642e028eae0c3f933e0021b19eee71b510d3
Maximillian Xavier <maximillianfx@gmail.com>
2020-03-14 18:38:22 -0300

    Add .dockerignore, configs to nginx, Dockerfile to frontend and adjust in env.prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  url_base: 'http://localhost:3000/movies/',
+  url_base_genre: 'http://localhost:3000/genres/',
+  url_base_poster: 'http://localhost:3000/poster',
+  url_base_backdrop: 'http://localhost:3000/backdrop'
 };
