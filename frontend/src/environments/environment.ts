20fcbf55dbcd572eee4efe27f01762c493b4302e
Maximillian Xavier <maximillianfx@gmail.com>
2020-03-15 17:49:30 -0300

    Update url with version

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  url_base: 'http://localhost:3000/movies/',
-  url_base_genre: 'http://localhost:3000/genres/',
-  url_base_poster: 'http://localhost:3000/poster',
-  url_base_backdrop: 'http://localhost:3000/backdrop'
+  url_base: 'http://localhost:3000/api/v1/movies/',
+  url_base_genre: 'http://localhost:3000/api/v1/genres/',
+  url_base_poster: 'http://localhost:3000/api/v1/poster',
+  url_base_backdrop: 'http://localhost:3000/api/v1/backdrop'
 };
 
 /*
